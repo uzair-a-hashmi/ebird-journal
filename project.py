2408c0ea9ed37829def6c11e1fa2fcea2ba1abba
Uzair Hashmi <hashmi.uzairahmad@gmail.com>
2026-06-21 21:40:44 -0500

    complete lookup and nearby functions and fall asleep from chronic boredom

--- project.py
+++ project.py
@@ -2,6 +2,8 @@ import argparse
 import csv
 import requests
 import json
+from pycountry import countries
+from urllib.parse import quote_plus
 from os import path
 from datetime import date as dt
 from tabulate import tabulate
@@ -26,23 +28,69 @@ def get_speciescode(common_name):
     response = requests.get("https://api.ebird.org/v2/ref/taxonomy/ebird?fmt=json")
     specieslist = response.json()
     for species in specieslist:
-        if common_name == species["comName"]:
+        if common_name.lower() == species["comName"].lower():
             return species["speciesCode"]
     return
 
 
 def lookup(species, regioncode):
     speciescode = get_speciescode(species)
-    if not speciescode:
-        return
-    api_key = "oalab1htmiag"
-    response = requests.get(f"https://api.ebird.org/v2/data/obs/{regioncode}/recent/{speciescode}?maxResults=10", headers={"x-ebirdapitoken": api_key})
-    observations = response.json()
-    for observation in observations:
-        yield f"⚝ Sighting! {observation["locName"]} at {observation["obsDt"]}"
+    country = countries.get(alpha_2=regioncode)
     
+    if not country:
+        message = f"'{regioncode}' is not a valid country code (must be ISO-2, case insensitive)"
+    elif not speciescode:
+        message = f"'{species}' is not a valid species name (must be common name, check eBird's list of common names)"
+    else:
+        api_key = "oalab1htmiag"
+        response = requests.get(f"https://api.ebird.org/v2/data/obs/{regioncode.upper()}/recent/{speciescode}?maxResults=10", headers={"x-ebirdapitoken": api_key})
+        observations = response.json()
+
+        if observations:
+            sightings = [f"{observation["obsDt"]}: {observation["comName"]} sighting at {observation["locName"]}" for observation in observations]
+            message = "\n".join(sightings)
+        else:
+            message = f"No {species} sightings in {country.name}"
+    return message
+
+
+def get_search_results(location):
+    # takes in search query, returns dictionary with "lat" and "lon" keys
+    location = quote_plus(location)
+    response = requests.get(f"https://nominatim.openstreetmap.org/search?q={location}&format=json", headers={"User-Agent": "eBird-Journal/1.0 (personal birdwatching project)"})
+    search_results = response.json()
+    if len(search_results) > 1:
+        print("Returned multiple results: ")
+        for i, result in enumerate(search_results):
+            print(f"({i + 1}): {result["display_name"]} - {result["addresstype"]}")
+        
+        while True:
+            try:
+                i = int(input("Which would you like to search? (#) ")) - 1
+            except ValueError:
+                pass
+            else:
+                if i in range(len(search_results)):
+                    return search_results[i]
+    if len(search_results) == 1:
+        return search_results[0]
+
+
 def nearby(location):
-    print(f"nearby function location triggered for {location}")
+    if search_result := get_search_results(location):
+        api_key = "oalab1htmiag"
+        response = requests.get(f"https://api.ebird.org/v2/data/obs/geo/recent?lat={search_result['lat']}&lng={search_result['lon']}&maxResults=10", headers={"x-ebirdapitoken": api_key})
+        observations = response.json()
+        if observations:
+            sightings = [f"{observation["obsDt"]}: {observation["comName"]} sighting at {observation["locName"]}" for observation in observations]
+            message = "\n".join(sightings)
+        else:
+            message = f"No recent sightings in {search_result["display_name"]}"
+    else: 
+        message = f"No search results came back for '{location}'"
+    return message
+
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -136,8 +184,9 @@ def main():
             else:
                 print("Your journal doesn't exist yet! Create one with the log command.")
         case "lookup":
-            for sighting in lookup(args.species, args.region):
-                print(sighting)
+            print(lookup(args.species, args.region))
+        case "nearby":
+            print(nearby(args.location))
 
 if __name__ == "__main__":
     main()
